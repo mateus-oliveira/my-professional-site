32ccbec8ae77b7d59f0eaf0e5c94f580dca64f83
mateus-oliveira <matews5522@gmail.com>
2020-04-28 12:20:23 -0300

    Loader

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import Loader from 'react-loader-spinner'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
+
+import './styles.css';
+
+import Header from '../../components/Header';
 
 export default class Loading extends Component {
-  render() {
-    return <div>Loading...</div>;
-  }
+    render() {
+        return (
+            <div className="loading">
+                <Header />
+                <div className="loader">
+                    <Loader
+                        type="Rings"
+                        color="#000"
+                        height={300}
+                        width={300}
+                        timeout={30000} 
+                    />
+                </div>
+            </div>
+        );
+    }
 }
