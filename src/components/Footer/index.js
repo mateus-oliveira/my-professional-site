347d9430699ba1dc39c28b428b4d69d491f30458
Mateus Oliveira <devmateusalves@gmail.com>
2023-06-17 11:13:01 -0300

    remove form email

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,44 +1,10 @@
 import React, { Component } from 'react';
-import emailjs from 'emailjs-com'
 
 import './styles.css';
 
-import {FiPhone, FiMail, FiSend, FiRotateCcw} from 'react-icons/fi';
+import {FiPhone, FiMail} from 'react-icons/fi';
 
 export default class Footer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-        }
-    }
-
-    handleSubmit = async event => {
-        event.preventDefault();
-
-        const { name, email, message } = this.state
-
-        if (!name || !email || !message) return;
-
-        let templateParams = {
-            from_name: email,
-            to_name: 'matews5522@gmail.com',
-            subject: `Site pessoal - Mensagem de ${name}.`,
-            message_html: `<p>${message}</p>`,
-        }
-        await emailjs.send(
-            'gmail',
-            'template_JLfQA7ww',
-            templateParams,
-            "user_W3N5gxqh4cqieREnXqf47"
-        )
-        this.resetForm();
-    };
-
-    resetForm = () => this.setState({name: '', email: '', message: ''})
-
     render() {
         return (
             <footer>
@@ -51,41 +17,6 @@ export default class Footer extends Component {
                         <div className='icon'><FiMail color="#FFF" size={22} /></div>
                         <h2>devmateusalves@gmail.com</h2>
                     </div>
-                </div>
-                <div className="contact_form">
-                    <form onSubmit={this.handleSubmit} method="POST">
-                        <h2>Contact me</h2>
-                        <input 
-                            type="text" 
-                            placeholder='Name' 
-                            value={this.state.name} 
-                            onChange={event => this.setState({name: event.target.value})} 
-                        />
-                        <input 
-                            type="email" 
-                            placeholder='Email' 
-                            aria-describedby="emailHelp" 
-                            value={this.state.email} 
-                            onChange={event => this.setState({email: event.target.value})} 
-                        />
-                        <textarea 
-                            rows="5" 
-                            value={this.state.message} 
-                            placeholder='Message'
-                            onChange={event => this.setState({message: event.target.value})} 
-                        />
-                        <div className="button">
-                            <button type="button" onClick={this.resetForm}>
-                                <FiRotateCcw className='form-icon'/>
-                                Reset
-                            </button>
-                            <button type="submit">
-                                <FiSend className='form-icon'/>
-                                Send
-                            </button>
-                        </div>
-                        
-                    </form>
                 </div>
             </footer>
         );
